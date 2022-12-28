58ea976f0d054c8e510bc8bb2f3985d2079e13e0
Everton <76123738+Vzuviiu@users.noreply.github.com>
2022-12-28 13:40:46 -0300

    Update PkmTamagochi.py

--- PkmTamagochi.py
+++ PkmTamagochi.py
@@ -2,12 +2,22 @@ import requests
 
 lista_pkm = []
 url = f'https://pokeapi.co/api/v2/pokemon/{id}/'
-for i in range(1):
-    pkm = 1
+pkm = 1
+
+
+
+for i in range(10):
     url = f'https://pokeapi.co/api/v2/pokemon/{pkm}/'
     response = requests.get(url)
     data = response.json()
     if response.status_code == 200:
-        print(f'Nome do Pokemon: {data["name"]}')
+        
+        lista_pkm.append(data['name'])
     else:
         print('Todos os pokemons morreram')
+    pkm = pkm + 1
+print(lista_pkm)
+print(' ')
+escolha = input('Escolha um pokemon\n')
+
+
